32927c1951989734a96c3ba4c40e120bf77f6e8a
pierpaolosestito200933 <sstppl99h29d122x@studenti.unical.it>
2022-11-27 16:41:54 +0100

    updated requirements and added value objects

---
+++ musics_library/domain.py
@@ -0,0 +1,105 @@
+from password_validator import PasswordValidator
+from typeguard import typechecked
+from dataclasses import dataclass
+from valid8 import validate
+from validation.regex import pattern
+
+@typechecked
+@dataclass(frozen=True,order=True)
+class Name:
+    value: str
+    def __post_init__(self):
+        validate('value', self.value, min_len=1, max_len=50, custom=pattern(r'[A-Za-z0-9- ,!@]*'))
+    def __str__(self):
+        return self.value
+
+@typechecked
+@dataclass(frozen=True,order=True)
+class Artist:
+    value:str
+
+    def __post_init__(self):
+        validate('value', self.value, min_len=1, max_len=50, custom=pattern(r'[A-Za-z0-9- ,!@]*'))
+
+    def __str__(self):
+        return self.value
+
+@typechecked
+@dataclass(frozen=True,order=True)
+class RecordCompany:
+    value:str
+
+    def __post_init__(self):
+        validate('value', self.value, min_len=1, max_len=50, custom=pattern(r'[A-Za-z0-9- ,!@#]*'))
+
+    def __str__(self):
+        return self.value
+
+@typechecked
+@dataclass(frozen=True,order=True)
+class Genre:
+    value: str
+
+    def __post_init__(self):
+        validate('value', self.value, min_len=1, max_len=25, custom=pattern(r'^[A-Z][A-Za-z ]*'))
+
+    def __str__(self):
+        return self.value
+
+@typechecked
+@dataclass(frozen=True,order=True)
+class EANCode:
+    pass
+
+
+
+@typechecked
+@dataclass(frozen=True,order=True)
+class Price:
+    pass
+
+#TODO Published-by, Created-at, Updated-At
+
+@typechecked
+@dataclass(frozen=True,order=True)
+class Email:
+    value: str
+
+    def __post_init__(self):
+        validate('value', self.value, min_len=1, max_len=150, custom=pattern(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'))
+
+    def __str__(self):
+        return self.value
+
+
+@typechecked
+@dataclass(frozen=True,order=True)
+class Username:
+    value:str
+
+    def __post_init__(self):
+        validate('value', self.value, min_len=1, max_len=150, custom=pattern(r'[A-Za-z0-9]*'))
+
+    def __str__(self):
+        return self.value
+
+
+@typechecked
+@dataclass(frozen=True,order=True)
+class Password:
+    value:str
+
+    def __post_init__(self):
+        schema = PasswordValidator()
+        schema\
+            .min(8)\
+            .max(128)\
+            .has().no().spaces()\
+            .has().uppercase()\
+            .has().lowercase()\
+            .has(r'[\@|\!|\.|\-]*')
+        validation = schema.validate(self.value)
+        if(not validation):
+            raise ValueError("Password isn't valid")
+
+
---
+++ tests/musics_library/test_domain.py
@@ -0,0 +1,132 @@
+from valid8 import ValidationError
+
+from musics_library.domain import Password,Name,Artist,RecordCompany,Genre,Username,Email
+import pytest
+
+
+#NAME
+def test_empty_name_raises_exception():
+    with pytest.raises(ValidationError):
+        Name("")
+
+def test_name_length_of_51_raises_exception():
+    with pytest.raises(ValidationError):
+        Name("A"*51)
+
+def test_name_accepts_some_symbols_like_space_minus_and_comma():
+    name_with_symbols = ['Blink!','AC-DC','Jelly, The Fish!',"@AM"]
+    for name in name_with_symbols:
+        assert Name(name)
+
+def test_name_str():
+    assert str(Name("SSDSBM")) == "SSDSBM"
+
+#ARTIST
+def test_empty_artist_name_raises_exception():
+    with pytest.raises(ValidationError):
+        Artist("")
+
+def test_artist_name_of_length_51_raises_exception():
+    with pytest.raises(ValidationError):
+        Artist("A"*51)
+
+def test_artist_name_accepts_some_symbols_like_space_minus_and_comma():
+    name_with_symbols = ['Blink!','AC-DC','Jelly, The Fish!',"@AM"]
+    for name in name_with_symbols:
+        assert Artist(name)
+
+def test_artist_name_str():
+    assert str(Artist("SSDSBM")) == "SSDSBM"
+
+#RECORDCOMPANY
+def test_empty_record_company_raises_exception():
+    with pytest.raises(ValidationError):
+        RecordCompany("")
+
+def test_record_company_of_length_51_raises_exception():
+    with pytest.raises(ValidationError):
+        RecordCompany("A"*51)
+
+def test_record_company_accepts_some_symbols_like_space_minus_and_comma():
+    name_with_symbols = ['Blink!','AC-DC','Jelly, The Fish!',"@AM","#MusiCEnTerTainMEnt"]
+    for name in name_with_symbols:
+        assert RecordCompany(name)
+
+def test_record_company_str():
+    assert str(RecordCompany("SSDSBM - Music Entertainment")) == "SSDSBM - Music Entertainment"
+#GENRE
+def test_empty_genre_raises_exception():
+    with pytest.raises(ValidationError):
+        Genre("")
+def test_genre_of_length_26_raises_exception():
+    with pytest.raises(ValidationError):
+        Genre("A"*26)
+
+def test_genre_accepts_only_whitespaces():
+    wrong_values = ["Rock!","Rock-n-Roll","Jazz!@","Blues1"]
+    for wrong in wrong_values:
+        with pytest.raises(ValidationError):
+            Genre(wrong)
+
+    correct_values = ["Rock","Rock n Roll","Folk","Jazz","Blues","Heavy Metal"]
+    for correct in correct_values:
+        assert Genre(correct)
+
+def test_genre_first_letter_is_upper():
+    with pytest.raises(ValidationError):
+        Genre("rock")
+    assert Genre("Rock")
+def test_genre_str():
+    assert str(Genre("Rock")) == "Rock"
+
+#USERNAME
+def test_empty_username_raises_exception():
+    with pytest.raises(ValidationError):
+        Username("")
+
+def test_username_of_length_151_raises_exception():
+    with pytest.raises(ValidationError):
+        Username("A"*151)
+
+def test_username_only_accepts_characters_and_numbers():
+    wrong_values = ["u'@-sername","u\sername123","u!ser"]
+    for wrong in wrong_values:
+        with pytest.raises(ValidationError):
+            Username(wrong)
+    correct_values = ['username123','Ricky1',"ssdsbm14"]
+    for correct in correct_values:
+        assert Username(correct)
+
+#EMAIL
+def test_empty_email_raises_exception():
+    with pytest.raises(ValidationError):
+        Email("")
+
+def test_email_of_length_151_raises_exception():
+    with pytest.raises(ValidationError):
+        Email("p"*146+"@a.it")
+
+def test_email_format():
+    wrong_values = ["wrong_email@it","anotherwrongemail.it"]
+    for wrong in wrong_values:
+        with pytest.raises(ValidationError):
+            Email(wrong)
+    correct_values = ["correct_email@gmail.com","correct.email@libero.it","correct12_email@yahoo.it"]
+    for correct in correct_values:
+        assert Email(correct)
+
+
+#PASSWORD
+@pytest.mark.parametrize("wrong_passwords", [("A"), ("aa@"), ("Aa "), ("A-!@"), ("Aaaaa"),("Aaa aa"), ("Aaaaaaa")])
+def test_password_has_min_length_of_8(wrong_passwords):
+    with pytest.raises(ValueError):
+        Password(wrong_passwords)
+    assert Password("Abcd!-@.")
+
+def test_password_length_of_129_raises_exception():
+    with pytest.raises(ValueError):
+        Password("A"*128+"a")
+
+def test_password_with_spaces_raises_exception():
+    with pytest.raises(ValueError):
+        Password("A"*6+" "+"a")
