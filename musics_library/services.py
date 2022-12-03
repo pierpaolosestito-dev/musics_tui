72b656006d5f0ed5d56a31c8843a14c81988b8c5
pierpaolosestito200933 <sstppl99h29d122x@studenti.unical.it>
2022-12-03 19:22:40 +0100

    added python-dotenv library to handle env variables

--- musics_library/services.py
+++ musics_library/services.py
@@ -1,10 +1,13 @@
-import json
-from datetime import datetime
 
 import requests
-import getpass
-
 from musics_library.domain import Username, Password, Music, EANCode, Genre, RecordCompany, Artist, Name, Price
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+music_endpoint = os.getenv('MUSIC_ENDPOINT')
+auth_endpoint = os.getenv('AUTH_ENDPOINT')
 
 class AuthenticatedUser:
     def __init__(self, key: str,id:int,username:str):
@@ -20,7 +23,7 @@ class ApiException(Exception):
 class LoginService:
     #User
     def login(self,username:Username,password:Password):
-        res = requests.post(url="http://localhost:8000/api/v1/auth/login/",json={"username":username.value\
+        res = requests.post(url=auth_endpoint+"login/",json={"username":username.value\
                                                                     ,"password":password.value})
         if res.status_code != 200:
             raise ApiException("Login not successfull")
@@ -28,14 +31,14 @@ class LoginService:
         return AuthenticatedUser(res.json()["key"],res.json()['user']['id'],res.json()['user']['username'])
 
     def logout(self,auth_user : AuthenticatedUser):
-        res = requests.post(url="http://localhost:8000/api/v1/auth/logout/",headers={'Authorization': f'Token {auth_user.key}'})
+        res = requests.post(url=auth_endpoint+"logout/",headers={'Authorization': f'Token {auth_user.key}'})
         if res.status_code != 200:
             raise ApiException("Logout not successfull")
         return res.json()
 
 class MusicsService:
     def fetch_musics_list(self,auth_user: AuthenticatedUser): #TODO Qua potremmo anche toglierlo auth_user perché le GET le facciamo fare anche a chi non è in possesso di un token.
-        res = requests.get(url="http://localhost:8000/api/v1/musics/",headers={'Authorization':f'Token {auth_user.key}'})
+        res = requests.get(url=music_endpoint,headers={'Authorization':f'Token {auth_user.key}'})
         if res.status_code != 200:
             raise ApiException("Request to server not successfull")
         return res.json() #TODO Deve essere una lista di Music
@@ -43,7 +46,7 @@ class MusicsService:
     def add_music(self,cd:Music,auth_user: AuthenticatedUser):
         dict = cd.toDict()
         dict['published_by'] = auth_user.id
-        res = requests.post(url="http://localhost:8000/api/v1/musics/",headers={'Authorization':f'Token {auth_user.key}'},
+        res = requests.post(url=music_endpoint,headers={'Authorization':f'Token {auth_user.key}'},
                             json=dict)
         if res.status_code != 201:
             raise ApiException("CD creation failed") ## todo bisogna leggere gli errori esatti e stamparli nella tui
@@ -59,7 +62,7 @@ class MusicsService:
 class MusicsByArtistService():
     #http://localhost:8000/api/v1/musics/byartist?artist=ciccio
     def fetch_musics_by_artist_list(self,artist_name:Artist,auth_user: AuthenticatedUser): #TODO Qua potremmo anche toglierlo auth_user perché le GET le facciamo fare anche a chi non è in possesso di un token.
-        res = requests.get(url="http://localhost:8000/api/v1/musics/byartist?artist="+artist_name.value,headers={'Authorization':f'Token {auth_user.key}'})
+        res = requests.get(url=music_endpoint+"byartist?artist="+artist_name.value,headers={'Authorization':f'Token {auth_user.key}'})
         if res.status_code != 200:
             raise ApiException("Request to server not successfull")
         return res.json()
@@ -67,7 +70,7 @@ class MusicsByArtistService():
 class MusicsByPublishedByService():
     # http://localhost:8000/api/v1/musics/by_published_by?published_by=ciccio
     def fetch_musics_by_published_by_list(self,published_by:Username,auth_user:AuthenticatedUser):
-        res = requests.get(url="http://localhost:8000/api/v1/musics/by_published_by?published_by=" + published_by.value,
+        res = requests.get(url=music_endpoint+"by_published_by?published_by=" + published_by.value,
                            headers={'Authorization': f'Token {auth_user.key}'})
         if res.status_code != 200:
             raise ApiException("Request to server not successfull")
@@ -76,7 +79,7 @@ class MusicsByPublishedByService():
 class MusicsByNameService():
     #http://localhost:8000/api/v1/musics/byname?name=ciccio
     def fetch_musics_by_name_list(self,cd_name:Name,auth_user:AuthenticatedUser):
-        res = requests.get(url="http://localhost:8000/api/v1/musics/byname?name=" + cd_name.value,
+        res = requests.get(url=music_endpoint+"byname?name=" + cd_name.value,
                            headers={'Authorization': f'Token {auth_user.key}'})
         if res.status_code != 200:
             raise ApiException("Request to server not successfull")
@@ -94,6 +97,4 @@ class MusicsByNameService():
 #
 # except(ApiException) as e:
 #     print(e)
-
-
-
+os.get
