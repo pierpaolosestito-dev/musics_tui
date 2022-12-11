34007ebf0fde4adf61ee4366ddee89972408536e
Fabrizio Marino <fb.marino@hotmail.it>
2022-12-11 16:55:55 +0100

    indentation fix

--- musics_library/services.py
+++ musics_library/services.py
@@ -1,27 +1,32 @@
-from dataclasses import dataclass,field
-from typing import List
-from typeguard import typechecked
-from musics_library.domain import Username, Password, Music, EANCode, Genre, RecordCompany, Artist, Name, Price, ID
-from dotenv import load_dotenv
-import musics_library.mappers as mappers
 import os
+from dataclasses import dataclass, field
+from typing import List
+
 import requests
+from dotenv import load_dotenv
+from typeguard import typechecked
+
+import musics_library.mappers as mappers
+from musics_library.domain import Username, Password, Music, Artist, Name, ID
 
 load_dotenv()
 
 music_endpoint = os.getenv('MUSIC_ENDPOINT')
 auth_endpoint = os.getenv('AUTH_ENDPOINT')
 
+
 class SingletonMeta(type):
     _instances = {}
+
     def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:
-            instance = super().__call__(*args,**kwargs)
+            instance = super().__call__(*args, **kwargs)
             cls._instances[cls] = instance
         return cls._instances[cls]
 
+
 class AuthenticatedUser(metaclass=SingletonMeta):
-    def __init__(self, key: str,id:ID,username:Username):
+    def __init__(self, key: str, id: ID, username: Username):
         self.key = key
         self.id = id
         self.username = username
@@ -33,6 +38,7 @@ class AuthenticatedUser(metaclass=SingletonMeta):
 class ApiException(Exception):
     pass
 
+
 CONNECTION_ERROR = "Check your network connection or retry later."
 LOGIN_ERROR = "Login not successfull"
 LOGOUT_ERROR = "Logout not successfull"
@@ -41,12 +47,13 @@ POST_ERROR = "MUSIC ADD FAILED"
 PUT_ERROR = "MUSIC UPDATE FAILED"
 DELETE_ERROR = "MUSIC DELETE FAILED"
 
+
 class AuthenticationService:
-    #User
-    def login(self,username:Username,password:Password):
+    # User
+    def login(self, username: Username, password: Password):
         try:
-            res = requests.post(url=auth_endpoint+"login/",json={"username":username.value\
-                                                                    ,"password":password.value})
+            res = requests.post(url=auth_endpoint + "login/", json={"username": username.value \
+                , "password": password.value})
         except:
             raise ApiException(CONNECTION_ERROR)
         if res.status_code != 200:
@@ -54,9 +61,9 @@ class AuthenticationService:
         authenticated_user = mappers.AuthenticatedUserMapper.map_auth_user(res)
         return authenticated_user
 
-    def logout(self,auth_user : AuthenticatedUser):
+    def logout(self, auth_user: AuthenticatedUser):
         try:
-            res = requests.post(url=auth_endpoint+"logout/",headers={'Authorization': f'Token {auth_user.key}'})
+            res = requests.post(url=auth_endpoint + "logout/", headers={'Authorization': f'Token {auth_user.key}'})
         except:
             raise ApiException(CONNECTION_ERROR)
         if res.status_code != 200:
@@ -64,24 +71,22 @@ class AuthenticationService:
         return res.json()
 
 
-
 class MusicsService:
-    #musicslibrary.domain import Music
+    # musicslibrary.domain import Music
     authenticated_user = AuthenticatedUser
 
-    def __to_dict(self, cd:Music):
-            return {
-                "name": cd.name.value,
-                "artist": cd.artist.value,
-                "record_company": cd.record_company.value,
-                "genre": cd.genre.value,
-                "ean_code": cd.ean_code.value,
-                "price": str(cd.price)
-            }
+    def __to_dict(self, cd: Music):
+        return {
+            "name": cd.name.value,
+            "artist": cd.artist.value,
+            "record_company": cd.record_company.value,
+            "genre": cd.genre.value,
+            "ean_code": cd.ean_code.value,
+            "price": str(cd.price)
+        }
 
-
-
-    def fetch_musics_list(self):#Lista di Musics -> un Array, o un List della libreria typing #TODO MIGLIORARE return e gestione errori
+    def fetch_musics_list(
+            self):  # Lista di Musics -> un Array, o un List della libreria typing #TODO MIGLIORARE return e gestione errori
         try:
             res = requests.get(url=music_endpoint)
         except:
@@ -93,14 +98,11 @@ class MusicsService:
             cd = mappers.MusicMapper.map_cd(i)
             cds.append(cd)
 
+        return cds  # TODO Deve essere una lista di Music
 
-        return cds #TODO Deve essere una lista di Music
-
-
-
-    def fetch_music_detail(self,cd_id:ID):
+    def fetch_music_detail(self, cd_id: ID):
         try:
-            res = requests.get(url=music_endpoint+str(cd_id.value)+"/")
+            res = requests.get(url=music_endpoint + str(cd_id.value) + "/")
         except:
             raise ApiException(CONNECTION_ERROR)
         if res.status_code != 200:
@@ -110,29 +112,29 @@ class MusicsService:
         cd = mappers.MusicMapper.map_cd(i)
         return cd
 
-    def add_music(self,cd:Music,auth_user: AuthenticatedUser):
+    def add_music(self, cd: Music, auth_user: AuthenticatedUser):
         dict = self.__to_dict(cd)
         dict['published_by'] = auth_user.id.value
 
         try:
-            res = requests.post(url=music_endpoint,headers={'Authorization':f'Token {auth_user.key}'},
-                            json=dict)
+            res = requests.post(url=music_endpoint, headers={'Authorization': f'Token {auth_user.key}'},
+                                json=dict)
         except:
             raise ApiException(CONNECTION_ERROR)
         if res.status_code != 201:
-            raise ApiException(POST_ERROR) ## todo bisogna leggere gli errori esatti e stamparli nella tui
+            raise ApiException(POST_ERROR)  ## todo bisogna leggere gli errori esatti e stamparli nella tui
         cd2 = mappers.MusicMapper.mappers.MusicMapper.map_cd(res.json())
         return cd2
 
-
-    def update_music(self,cd:Music,auth_user : AuthenticatedUser):
+    def update_music(self, cd: Music, auth_user: AuthenticatedUser):
         dict = self.__to_dict(cd)
         dict['id'] = cd.id.value
         dict['published_by'] = auth_user.id.value
         try:
 
-            res = requests.put(url=music_endpoint+str(cd.id)+"/",headers={'Authorization':f'Token {auth_user.key}'},
-                           json=dict)
+            res = requests.put(url=music_endpoint + str(cd.id) + "/",
+                               headers={'Authorization': f'Token {auth_user.key}'},
+                               json=dict)
         except:
             raise ApiException(CONNECTION_ERROR)
 
@@ -140,9 +142,10 @@ class MusicsService:
             raise ApiException(PUT_ERROR)
         return True
 
-    def remove_music(self,cd_id:ID,auth_user:AuthenticatedUser):
+    def remove_music(self, cd_id: ID, auth_user: AuthenticatedUser):
         try:
-            res = requests.delete(url=music_endpoint+str(cd_id.value)+"/",headers={'Authorization':f'Token {auth_user.key}'})
+            res = requests.delete(url=music_endpoint + str(cd_id.value) + "/",
+                                  headers={'Authorization': f'Token {auth_user.key}'})
         except:
             raise ApiException(CONNECTION_ERROR)
         if res.status_code != 204:
@@ -151,10 +154,11 @@ class MusicsService:
 
 
 class MusicsByArtistService():
-    #http://localhost:8000/api/v1/musics/byartist?artist=ciccio
-    def fetch_musics_by_artist_list(self,artist_name:Artist): #TODO Qua potremmo anche toglierlo auth_user perché le GET le facciamo fare anche a chi non è in possesso di un token.
+    # http://localhost:8000/api/v1/musics/byartist?artist=ciccio
+    def fetch_musics_by_artist_list(self,
+                                    artist_name: Artist):  # TODO Qua potremmo anche toglierlo auth_user perché le GET le facciamo fare anche a chi non è in possesso di un token.
         try:
-            res = requests.get(url=music_endpoint+"byartist?artist="+artist_name.value)
+            res = requests.get(url=music_endpoint + "byartist?artist=" + artist_name.value)
         except:
             raise ApiException(CONNECTION_ERROR)
         if res.status_code != 200:
@@ -164,12 +168,13 @@ class MusicsByArtistService():
             cd = mappers.MusicMapper.map_cd(i)
             cds.append(cd)
         return cds
+
 
 class MusicsByPublishedByService():
     # http://localhost:8000/api/v1/musics/by_published_by?published_by=ciccio
-    def fetch_musics_by_published_by_list(self,published_by:Username):
+    def fetch_musics_by_published_by_list(self, published_by: Username):
         try:
-            res = requests.get(url=music_endpoint+"by_published_by?publishedby=" + published_by.value)
+            res = requests.get(url=music_endpoint + "by_published_by?publishedby=" + published_by.value)
         except:
             raise ApiException(CONNECTION_ERROR)
         if res.status_code != 200:
@@ -180,11 +185,12 @@ class MusicsByPublishedByService():
             cds.append(cd)
         return cds
 
+
 class MusicsByNameService():
-    #http://localhost:8000/api/v1/musics/byname?name=ciccio
-    def fetch_musics_by_name_list(self,cd_name:Name):
+    # http://localhost:8000/api/v1/musics/byname?name=ciccio
+    def fetch_musics_by_name_list(self, cd_name: Name):
         try:
-            res = requests.get(url=music_endpoint+"byname?name=" + cd_name.value)
+            res = requests.get(url=music_endpoint + "byname?name=" + cd_name.value)
         except:
             raise ApiException(CONNECTION_ERROR)
         if res.status_code != 200:
@@ -194,40 +200,37 @@ class MusicsByNameService():
             cd = mappers.MusicMapper.map_cd(i)
             cds.append(cd)
         return cds
+
 
 @typechecked
 @dataclass(frozen=True)
 class MusicLibrary:
-
-
-    musics_service: MusicsService = field(default_factory=MusicsService,init=False)
-    musics_by_artists_service : MusicsByArtistService = field(default_factory=MusicsByArtistService,init=False)
-    musics_by_published_by_service : MusicsByPublishedByService = field(default_factory=MusicsByPublishedByService,init=False)
-    musics_by_cd_name_service : MusicsByNameService = field(default_factory=MusicsByNameService,init=False)
-
+    musics_service: MusicsService = field(default_factory=MusicsService, init=False)
+    musics_by_artists_service: MusicsByArtistService = field(default_factory=MusicsByArtistService, init=False)
+    musics_by_published_by_service: MusicsByPublishedByService = field(default_factory=MusicsByPublishedByService,
+                                                                       init=False)
+    musics_by_cd_name_service: MusicsByNameService = field(default_factory=MusicsByNameService, init=False)
 
     def musics(self) -> 'List[Music]':
         return self.musics_service.fetch_musics_list()
 
-    def music(self,id:ID) -> 'Music':
+    def music(self, id: ID) -> 'Music':
         return self.musics_service.fetch_music_detail(id)
 
-    def add_music(self,music:Music,auth_user:AuthenticatedUser) -> 'Music':
-        return self.musics_service.add_music(music,auth_user)
+    def add_music(self, music: Music, auth_user: AuthenticatedUser) -> 'Music':
+        return self.musics_service.add_music(music, auth_user)
 
-    def update_music(self,music:Music,auth_user:AuthenticatedUser) -> bool:
-        return self.musics_service.update_music(music,auth_user)
+    def update_music(self, music: Music, auth_user: AuthenticatedUser) -> bool:
+        return self.musics_service.update_music(music, auth_user)
 
-    def remove_music(self,id:ID,auth_user:AuthenticatedUser) -> bool:
-        return self.musics_service.remove_music(id,auth_user)
+    def remove_music(self, id: ID, auth_user: AuthenticatedUser) -> bool:
+        return self.musics_service.remove_music(id, auth_user)
 
-    def musics_by_artist(self,artist:Artist)->'List[Music]':
+    def musics_by_artist(self, artist: Artist) -> 'List[Music]':
         return self.musics_by_artists_service.fetch_musics_by_artist_list(artist)
 
-    def musics_by_published_by(self,published_by:Username)->'List[Music]':
+    def musics_by_published_by(self, published_by: Username) -> 'List[Music]':
         return self.musics_by_published_by_service.fetch_musics_by_published_by_list(published_by)
 
-    def musics_by_cd_name(self,cd_name:Name)->'List[Music]':
+    def musics_by_cd_name(self, cd_name: Name) -> 'List[Music]':
         return self.musics_by_cd_name_service.fetch_musics_by_name_list(cd_name)
-
-
